35ec4d8ce04f14d1aab632f2ca442e96944f36ff
Amanda Rangel <amandarangelaraujo@gmail.com>
2023-09-29 10:07:49 -0300

    first class Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,11 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
     public static void main (String[] args){
-        System.out.println("oi");
+        Position pos = new Position(3, 5);
+        System.out.println(pos);
 
     }
 }
